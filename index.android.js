fc3c641adedd84007efd21cdaf7b78ea4865144e
Luy Tran <octopitus@gmail.com>
2017-03-16 08:30:16 +0700

    add eslint & fix some errors

--- index.android.js
+++ index.android.js
@@ -4,13 +4,13 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
 export default class RNChallenge extends Component {
   render() {
@@ -27,7 +27,7 @@ export default class RNChallenge extends Component {
           Shake or press menu button for dev menu
         </Text>
       </View>
-    );
+    )
   }
 }
 
@@ -36,18 +36,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
 
-AppRegistry.registerComponent('RNChallenge', () => RNChallenge);
+AppRegistry.registerComponent('RNChallenge', () => RNChallenge)
